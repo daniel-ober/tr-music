634bd9fb59db7db897ae60834de9ba8ce4ea4c69
Dan Ober <danober.dev@gmail.com>
2026-01-09 00:34:42 -0600

    Wire app routing and remove default Vite App

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "4rem", textAlign: "center" }}>
+      <h1>Tawnya Reynolds</h1>
+      <p>Songwriter · Nashville</p>
+    </div>
+  );
+}
